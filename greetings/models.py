a1b1b37280ea4d2a3f23cd0b6fb516b1c741bade
Aleksey <veemer@yandex.ru>
2014-10-12 23:55:35 +0400

    create greetings app

---
+++ greetings/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=128)
+    parent = models.ForeignKey(Category, blank=True, null=True)
+
+
+class Greeting(models.Model):
+
+    category = models.ForeignKey(Category)
+    text = models.TextField()
