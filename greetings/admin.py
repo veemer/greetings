73840e8cad8d9df4cfa736dbae6b3a77fe6ee410
Aleksey <veemer@yandex.ru>
2014-10-13 00:25:00 +0400

    register models in admin

--- greetings/admin.py
+++ greetings/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from greetings.models import Category, Greeting
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    model = Category
+
+
+class GreetingAdmin(admin.ModelAdmin):
+
+    def teaser(self, greeting):
+        return greeting.text[:100]
+
+    model = Greeting
+    list_display = ('id', 'category', 'teaser')
+    list_filter = ('category',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Greeting, GreetingAdmin)
