f85d76b857176444ede38a9ff626e99e781346f1
Aleksey <veemer@yandex.ru>
2014-10-12 23:58:48 +0400

    import production_settings.py

--- androidgreetings/settings.py
+++ androidgreetings/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'greetings',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from production_settings import *
+except ImportError:
+    pass
