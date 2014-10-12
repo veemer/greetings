461ad974731fb62e120111b2fa21857ae9fb7de7
Aleksey <veemer@yandex.ru>
2014-10-13 00:52:25 +0400

    add __unicode__ for models

--- greetings/models.py
+++ greetings/models.py
@@ -8,8 +8,14 @@ class Category(models.Model):
     name = models.CharField(max_length=128)
     parent = models.ForeignKey('Category', blank=True, null=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Greeting(models.Model):
 
     category = models.ForeignKey(Category)
     text = models.TextField()
+
+    def __unicode__(self):
+        return self.text[:100]
