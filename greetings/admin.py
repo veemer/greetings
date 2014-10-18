8df940514f82804a3005e6b4a3e99fd167745970
Aleksey <veemer@yandex.ru>
2014-10-18 23:39:40 +0400

    update admin filters

--- greetings/admin.py
+++ greetings/admin.py
@@ -1,11 +1,40 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
 from greetings.models import Category, Greeting
 
 
+class RootCatsFilter(admin.SimpleListFilter):
+
+    title = u'Categories'
+    parameter_name = u'parent'
+
+    def lookups(self, request, model_admin):
+        cats = Category.objects.filter(parent__isnull=True)
+        filters = [('root', 'Roots')]
+        filters += [(cat.id, cat.name) for cat in cats]
+
+        return filters
+
+    def queryset(self, request, queryset):
+        cat = self.value()
+        if cat == 'root':
+            return queryset.filter(parent__isnull=True)
+        else:
+            return queryset.filter(parent=cat)
+
+
 class CategoryAdmin(admin.ModelAdmin):
 
+    def greetings_link(category):
+        return '<a href="/admin/greetings/greeting/?category__id__exact={}">Поздравления</a>'.format(category.id)
+    greetings_link.allow_tags = True
+
     model = Category
+    list_display = ('name', greetings_link)
+    search_fields = ('name',)
+    list_filter = (RootCatsFilter,)
 
 
 class GreetingAdmin(admin.ModelAdmin):
@@ -14,8 +43,9 @@ class GreetingAdmin(admin.ModelAdmin):
         return greeting.text[:100]
 
     model = Greeting
-    list_display = ('id', 'category', 'teaser')
+    list_display = ('teaser', 'category')
     list_filter = ('category',)
+    search_fields = ('text',)
 
 
 admin.site.register(Category, CategoryAdmin)
