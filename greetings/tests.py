0a9e659948dc3d68382cfb3520e611d4300ab88a
Aleksey <veemer@yandex.ru>
2014-10-21 21:42:59 +0400

    add test for GreetingsList

--- greetings/tests.py
+++ greetings/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 from greetings.models import Category, Greeting
+from greetings.views import GreetingsList
 
 # Create your tests here.
 
@@ -45,3 +46,27 @@ class TestViews(TestCase):
         
         self.assertIn(root_cat, response.context['root_cats'])
 
+    def test_greetings_list_view(self):
+        
+        root_cat = Category.objects.create(name='root cat')
+        child_cat = Category.objects.create(name='child cat', parent=root_cat)
+
+        url = reverse('greetings', args=(child_cat.id,))
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        greetings = []
+        for i in range(GreetingsList.paginate_by * 2, 0, -1):
+            greetings.append(Greeting.objects.create(text='greeting {}'.format(i), category=child_cat, sort=i))
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['greetings']), GreetingsList.paginate_by)
+
+        for i in range(0, GreetingsList.paginate_by):
+            self.assertEqual(response.context['greetings'][i], greetings[i])
+
+        self.assertEqual(response.context['root_cat'], root_cat)
+        self.assertIn(root_cat, response.context['root_cats'])
+        self.assertEqual(response.context['child_cat'], child_cat)
