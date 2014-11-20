ffb15ef51739c7238a8f627a249250ea2d21458c
Aleksey <veemer@yandex.ru>
2014-11-20 22:34:40 +0300

    update templates

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from blog.views import PostsList, PostDetail
+
+urlpatterns = patterns('',
+    url(r'^$', PostsList.as_view(), name='posts_list'),
+    url(r'(?P<pk>\d+)/$', PostDetail.as_view(), name='posts_list'),
+)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from blog.models import Post
+
+
+class PostsList(ListView):
+
+	model = Post
+	per_page = 20
+	template_name = 'blog/posts_list.html'
+	context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+
+	model = Post
+	template_name = 'blog/post_detail.html'
+	context_object_name = 'post'
